0d7e6400b56acee177cc73147c22da7072262625
leonstefano03 <leonstefano03@gmail.com>
2024-05-19 20:37:47 -0300

    feat:change-link-cv

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import astroBall from "../assets/astroBall.png";
-import { Link } from "react-router-dom";
-import "../style/AboutMe.css";
+import React from 'react';
+import astroBall from '../assets/astroBall.png';
+import { Link } from 'react-router-dom';
+import '../style/AboutMe.css';
 
 function AboutMe() {
   return (
@@ -32,7 +32,7 @@ function AboutMe() {
         <div className="div-button">
           <div className="btn-container">
             <Link
-              to="https://drive.google.com/file/d/1xJt4ihjvr5jestYS--aievzXBTpa3TcN/view?usp=sharing"
+              to="https://drive.google.com/file/d/1nP2PWUdh_iTtiTHr67be_67g9ICKqf-Q/view?usp=sharing"
               target="_blank"
             >
               <button
@@ -49,316 +49,316 @@ function AboutMe() {
                 <div
                   className="star"
                   style={{
-                    left: "119.273px",
-                    top: "18.0747px",
-                    animationDelay: "3.37051s",
-                    transform: "scale(1.1)",
+                    left: '119.273px',
+                    top: '18.0747px',
+                    animationDelay: '3.37051s',
+                    transform: 'scale(1.1)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "22.2022px",
-                    top: "10.69534px",
-                    animationDelay: "4.9s",
-                    transform: "scale(2.22)",
+                    left: '22.2022px',
+                    top: '10.69534px',
+                    animationDelay: '4.9s',
+                    transform: 'scale(2.22)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "14.754px",
-                    top: "25.2924px",
-                    animationDelay: "0.05s",
-                    transform: "scale(0.1)",
+                    left: '14.754px',
+                    top: '25.2924px',
+                    animationDelay: '0.05s',
+                    transform: 'scale(0.1)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "95.948px",
-                    top: "54.8942px",
-                    animationDelay: "3.1s",
-                    transform: "scale(1.87)",
+                    left: '95.948px',
+                    top: '54.8942px',
+                    animationDelay: '3.1s',
+                    transform: 'scale(1.87)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "184.622px",
-                    top: "20.0923px",
-                    animationDelay: "2.8s",
-                    transform: "scale(1.77)",
+                    left: '184.622px',
+                    top: '20.0923px',
+                    animationDelay: '2.8s',
+                    transform: 'scale(1.77)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "142.1px",
-                    top: "22.3542px",
-                    animationDelay: "2.73988s",
-                    transform: "scale(1.62715)",
+                    left: '142.1px',
+                    top: '22.3542px',
+                    animationDelay: '2.73988s',
+                    transform: 'scale(1.62715)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "145.079px",
-                    top: "6.97553px",
-                    animationDelay: "0.0408754s",
-                    transform: "scale(0.468075)",
+                    left: '145.079px',
+                    top: '6.97553px',
+                    animationDelay: '0.0408754s',
+                    transform: 'scale(0.468075)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "42.2308px",
-                    top: "9.78383px",
-                    animationDelay: "4.58407s",
-                    transform: "scale(0.0422065)",
+                    left: '42.2308px',
+                    top: '9.78383px',
+                    animationDelay: '4.58407s',
+                    transform: 'scale(0.0422065)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "91.2734px",
-                    top: "14.0408px",
-                    animationDelay: "2.05927s",
-                    transform: "scale(0.11997)",
+                    left: '91.2734px',
+                    top: '14.0408px',
+                    animationDelay: '2.05927s',
+                    transform: 'scale(0.11997)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "35.6985px",
-                    top: "52.6403px",
-                    animationDelay: "3.07343s",
-                    transform: "scale(0.672992)",
+                    left: '35.6985px',
+                    top: '52.6403px',
+                    animationDelay: '3.07343s',
+                    transform: 'scale(0.672992)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "76.4191px",
-                    top: "48.453px",
-                    animationDelay: "2.35679s",
-                    transform: "scale(2.001)",
+                    left: '76.4191px',
+                    top: '48.453px',
+                    animationDelay: '2.35679s',
+                    transform: 'scale(2.001)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "154.027px",
-                    top: "45.9848px",
-                    animationDelay: "3.723s",
-                    transform: "scale(1.4118)",
+                    left: '154.027px',
+                    top: '45.9848px',
+                    animationDelay: '3.723s',
+                    transform: 'scale(1.4118)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "110.241px",
-                    top: "20.2684px",
-                    animationDelay: "2.94906s",
-                    transform: "scale(1.2193)",
+                    left: '110.241px',
+                    top: '20.2684px',
+                    animationDelay: '2.94906s',
+                    transform: 'scale(1.2193)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "12.602px",
-                    top: "19.8836px",
-                    animationDelay: "4.072s",
-                    transform: "scale(1.49026)",
+                    left: '12.602px',
+                    top: '19.8836px',
+                    animationDelay: '4.072s',
+                    transform: 'scale(1.49026)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "30.0911px",
-                    top: "37.9746px",
-                    animationDelay: "1.02002s",
-                    transform: "scale(1.41008)",
+                    left: '30.0911px',
+                    top: '37.9746px',
+                    animationDelay: '1.02002s',
+                    transform: 'scale(1.41008)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "62.3096px",
-                    top: "9.64604px",
-                    animationDelay: "3.9445s",
-                    transform: "scale(0.231214)",
+                    left: '62.3096px',
+                    top: '9.64604px',
+                    animationDelay: '3.9445s',
+                    transform: 'scale(0.231214)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "44.7189px",
-                    top: "32.4307px",
-                    animationDelay: "4.78921s",
-                    transform: "scale(0.359408)",
+                    left: '44.7189px',
+                    top: '32.4307px',
+                    animationDelay: '4.78921s',
+                    transform: 'scale(0.359408)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "191.866px",
-                    top: "27.151px",
-                    animationDelay: "1.34451s",
-                    transform: "scale(1.13484)",
+                    left: '191.866px',
+                    top: '27.151px',
+                    animationDelay: '1.34451s',
+                    transform: 'scale(1.13484)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "47.6744px",
-                    top: "3.00604px",
-                    animationDelay: "1.04567s",
-                    transform: "scale(0.682023)",
+                    left: '47.6744px',
+                    top: '3.00604px',
+                    animationDelay: '1.04567s',
+                    transform: 'scale(0.682023)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "98.6225px",
-                    top: "49.6115px",
-                    animationDelay: "2.41384s",
-                    transform: "scale(1.96254)",
+                    left: '98.6225px',
+                    top: '49.6115px',
+                    animationDelay: '2.41384s',
+                    transform: 'scale(1.96254)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "57.4785px",
-                    top: "29.6588px",
-                    animationDelay: "3.3569s",
-                    transform: "scale(1.53118)",
+                    left: '57.4785px',
+                    top: '29.6588px',
+                    animationDelay: '3.3569s',
+                    transform: 'scale(1.53118)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "13.2213px",
-                    top: "24.538px",
-                    animationDelay: "1.69582s",
-                    transform: "scale(1.6236)",
+                    left: '13.2213px',
+                    top: '24.538px',
+                    animationDelay: '1.69582s',
+                    transform: 'scale(1.6236)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "56.9067px",
-                    top: "51.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    left: '56.9067px',
+                    top: '51.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "139.8361px",
-                    top: "45.3876px",
-                    animationDelay: "1.28648s",
-                    transform: "scale(0.566118)",
+                    left: '139.8361px',
+                    top: '45.3876px',
+                    animationDelay: '1.28648s',
+                    transform: 'scale(0.566118)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "40.9067px",
-                    top: "21.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    right: '40.9067px',
+                    top: '21.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "30.9067px",
-                    top: "26.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    right: '30.9067px',
+                    top: '26.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "30.9067px",
-                    top: "16.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    right: '30.9067px',
+                    top: '16.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "20.9067px",
-                    top: "10.904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    right: '20.9067px',
+                    top: '10.904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "40.9067px",
-                    top: "8.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    right: '40.9067px',
+                    top: '8.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "80.9067px",
-                    top: "8.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    left: '80.9067px',
+                    top: '8.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "100.9067px",
-                    top: "6.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    left: '100.9067px',
+                    top: '6.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    left: "130.9067px",
-                    top: "8.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(0.749788)",
+                    left: '130.9067px',
+                    top: '8.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(0.749788)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "80.9067px",
-                    top: "5.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(1.9999998)",
+                    right: '80.9067px',
+                    top: '5.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(1.9999998)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "45.9067px",
-                    top: "35.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(1.999998)",
+                    right: '45.9067px',
+                    top: '35.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(1.999998)',
                   }}
                 ></div>
                 <div
                   className="star"
                   style={{
-                    right: "85.9067px",
-                    top: "44.9904px",
-                    animationDelay: "4.74375s",
-                    transform: "scale(1.99998998)",
+                    right: '85.9067px',
+                    top: '44.9904px',
+                    animationDelay: '4.74375s',
+                    transform: 'scale(1.99998998)',
                   }}
                 ></div>
               </button>
